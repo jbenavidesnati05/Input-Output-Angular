b5bf439825e88ac135b5e5be480c7d473f3f62ca
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-03-07 11:25:32 -0500

    validaciones eliminar ok

--- src/app/componentes/padre-ouput/padre-ouput.component.ts
+++ src/app/componentes/padre-ouput/padre-ouput.component.ts
@@ -10,11 +10,20 @@ export class PadreOuputComponent implements OnInit {
   colores = ["azul", "rojo","amarillo"]
 
   agregarColor(color:string){
+
+    let encontrado = this.colores.find(item =>item == color)
+
     if(color == "")
-    alert("ingresar un color")
-    else{
+      alert("ingresar un color")
+    if(encontrado){
+      alert("El color ingresado ya existe")
+    }else{
       this.colores.push(color)
     }
+  }
+
+  eliminarColor(i:any){
+    this.colores.splice(i,1)
   }
 
   constructor() { }
